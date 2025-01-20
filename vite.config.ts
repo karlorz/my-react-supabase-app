b4baa08225a9b2af558ee9f51ab6d061d83daebc
karlorz <142769090+karlorz@users.noreply.github.com>
2025-01-20 23:41:37 +0800

    copy index to dist 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import { copyFileSync } from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "copy-index-to-404",
+      apply: "build",
+      closeBundle: () => {
+        // copy index.html to 404.html
+        copyFileSync(resolve(__dirname, "dist/index.html"), resolve(__dirname, "dist/404.html"));
+      },
+    },
+  ],
   envDir: "./env",
   // base: process.env.CI ? "/my-react-supabase-app/" : "/",
 });
