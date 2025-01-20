05b2bcad8828659dedc72ca44ee15d4b4d7dc714
karlorz <142769090+karlorz@users.noreply.github.com>
2025-01-20 23:14:01 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   envDir: "./env",
-  base: process.env.CI ? "/my-react-supabase-app/" : "/",
+  // base: process.env.CI ? "/my-react-supabase-app/" : "/",
 });
